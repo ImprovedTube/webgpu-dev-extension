a0d95ca2db8e614ac529fb4d9a0e5c810730d057
Gregg Tavares <github@greggman.com>
2023-11-08 17:58:44 +0900

    fixup error handling

--- extension/scripts/show-errors.js
+++ extension/scripts/show-errors.js
@@ -1,17 +1,25 @@
 if (typeof GPUDevice !== 'undefined') {
+  const errorScopeStack/*: {type: GPUErrorFilter, errors: GPUError[]}[]*/ = [];
+  const origPushErrorScope = GPUDevice.prototype.pushErrorScope;
+  const origPopErrorScope = GPUDevice.prototype.popErrorScope;
+
   function addErrorWrapper(api, fnName) {
     const origFn = api.prototype[fnName];
     api.prototype[fnName] = function(...args) {
       const stack = new Error();
-      const argsCopy = [...args];
-      this.pushErrorScope('validation');
+      origPushErrorScope.call(this, 'validation');
       const result = origFn.call(this, ...args);
-      this.popErrorScope()
-         .then(err => {
-          if (err) {
-            console.error(fnName, ...args);
-            console.error(err.message);
+      origPopErrorScope.call(this)
+        .then(error => {
+          if (error) {
+            console.error(fnName, args);
+            console.error(error.message);
             console.error(stack.stack);
+            if (errorScopeStack.length > 0) {
+              errorScopeStack[errorScopeStack.length - 1].errors.push(error);
+            } else {
+              this.dispatchEvent(new GPUUncapturedErrorEvent('validation', { error }));
+            }
           }
          });
       return result;
@@ -32,6 +40,26 @@ if (typeof GPUDevice !== 'undefined') {
   getAPIFunctionNames(GPUDevice)
     .filter(n => !skip.has(n))
     .forEach(n => addErrorWrapper(GPUDevice, n));
+
+  GPUDevice.prototype.pushErrorScope = (function(origFn) {
+    return function(/*this: GPUDevice,*/ type/*: GPUErrorFilter*/) {
+      origFn.call(this, type);
+      errorScopeStack.push({type, errors: []});
+    };
+  })(GPUDevice.prototype.pushErrorScope)
+
+  GPUDevice.prototype.popErrorScope = (function(origFn) {
+    return async function(/*this: GPUDevice*/) {
+      const errorScope = errorScopeStack.pop();
+      if (errorScope === undefined) {
+        const error = new Error();
+        error.name = 'OperationError';
+        return Promise.reject(error);
+      }
+      const err = await origFn.call(this);
+      return errorScope.errors.length > 0 ? errorScope.errors[0] : err;
+    };
+  })(GPUDevice.prototype.popErrorScope)
 
   GPUAdapter.prototype.requestDevice = (function(origFn) {
     return async function(...args) {
